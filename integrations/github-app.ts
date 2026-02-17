1340397c98ec2b7a0713e69331cb72995f8ff1a1
Daniel Lamando <dan@danopia.net>
2026-02-17 12:57:28 +0100

    1.0.6: Add crypto/rsa-import, integrations/github-app (#16)
    * Create github-app.ts with RSA private key
    
    * Break apart RSA private key import function
    
    * v1.0.6
    
    * relock
    
    * Add return type to fetchInstallationToken

---
+++ integrations/github-app.ts
@@ -0,0 +1,63 @@
+import {
+  create as createJwt,
+  getNumericDate,
+} from '@zaubrik/djwt';
+import { LogicTracer } from "@cloudydeno/opentelemetry/instrumentation/async.ts";
+
+const tracer = new LogicTracer({ name: 'github-app' });
+
+type GithubToken = {
+  token: string;
+  expires_at: string;
+}
+
+export class GithubAppIdentity {
+  constructor(
+    public readonly clientId: string,
+    private readonly privateKey: CryptoKey,
+  ) {}
+
+  @tracer.InternalSpan
+  async createGithubJwt(): Promise<string> {
+    return await createJwt({
+      'alg': 'RS256',
+      'typ': 'JWT',
+    }, {
+      'iss': this.clientId,
+      'exp': getNumericDate(5 * 60),
+      'iat': getNumericDate(0),
+      'nbf': getNumericDate(-5),
+    }, this.privateKey);
+  }
+
+  @tracer.InternalSpan
+  async fetchInstallationCredential(
+    installationId: string,
+    params: {
+      repositories?: string[],
+      permissions?: Record<string, 'read' | 'write' | undefined>,
+    },
+  ): Promise<GithubToken> {
+    const resp = await fetch(`https://api.github.com/app/installations/${installationId}/access_tokens`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/vnd.github+json',
+        'authorization': `Bearer ${await this.createGithubJwt()}`,
+        'X-GitHub-Api-Version': '2022-11-28',
+      },
+      body: JSON.stringify(params),
+    });
+    if (!resp.ok) {
+      console.error(JSON.stringify({
+        message: `Github returned HTTP ${resp.status} when creating installation token`,
+        requestId: resp.headers.get('x-github-request-id'),
+        responseType: resp.headers.get('content-type'),
+        responseBody: (await resp.text()).slice(0, 100),
+      }));
+      throw new Error(`HTTP ${resp.status} from Github when creating installation token`);
+    }
+    const data = await resp.json();
+    return data as GithubToken;
+  }
+}
